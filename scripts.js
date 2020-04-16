8fed63a8dcdfbece8dd20e8f47dc82377e4192ae
albertkomlos <63658389+albertkomlos@users.noreply.github.com>
2020-04-16 19:05:57 +0200

    condition

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ let name='Adam';
 console.log(`Hello my dear" ${name}`)
 
 //jQuery part
+
 $('p').html('The <strong>jQuery</strong> part has started')
 
 $('h1').css('background','red')
@@ -12,13 +13,21 @@ $('h2').toggleClass('green');
 $('p').toggleClass('green');
 $('p').toggleClass('green');
 
-$('main').append('<p id="first">Added element</p>');
-$('main').append('<p id="second">Added second element</p>');
+// $('main').append('<p id="first">Added element</p>');
+// $('main').append('<p id="second">Added second element</p>');
 
 $('#first').remove();
 
+
+//buttons
+
+//akkor addolunk uj p taget ha meg nincs legalabb 5
 $('#add-button').click(() => {
-    $('main').append('<p>Added a new p tag</p>');
+    if($('main p').length < 5) {
+        $('main').append('<p>Added a new p tag</p>');
+    } else {
+        $('main').append('<p class="green">Added a new p tag</p>');
+    }
 });
 
 
@@ -28,3 +37,15 @@ let removeFunction = () => {
 }
 
 $('#remove-button').click(removeFunction);
+
+
+
+// Fourth part: loops and conditions
+
+let age = 35;
+
+if (age === 18) {
+    console.log('This person can drink')
+} else {
+    console.log('This is a kid, no alcohol')
+}
